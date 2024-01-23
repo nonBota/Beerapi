a5777215889181c1eb2d8ba5a6529a0ed8a7c6d1
Srbottine <lucasbottine7@gmail.com>
2024-01-22 22:33:00 -0300

    alteração do controller

--- src/main/java/net/atos/beerapi/controller/BeerController.java
+++ src/main/java/net/atos/beerapi/controller/BeerController.java
@@ -1,14 +1,20 @@
 package net.atos.beerapi.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import net.atos.beerapi.model.Beer;
 import net.atos.beerapi.service.BeerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/beers")
+@Tag(name= "Beer API", description = "API para gerenciamento das cervejas que eu gosto!!!")
 public class BeerController {
 
     private final BeerService beerService;
@@ -18,7 +24,14 @@ public class BeerController {
         this.beerService = beerService;
     }
 
-    @GetMapping
+    @GetMapping(produces = "application/json")
+    @Operation(
+            summary = "Lista todas as cerveja",
+            description = "Lista todas as cervejas cadastradas no sistema!"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "operação com sucesso | []")
+    })
     public List<Beer> getAllBeer() {
         return beerService.getAllBeers();
     }
